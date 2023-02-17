4e7288d514d9e7859b717dab0146dfed481a6514
EdwinRA240 <edwinra240@gmail.com>
2023-02-17 08:06:18 -0600

    fix: traspaso Registro Pat

--- src/pages/RegistroPat.jsx
+++ src/pages/RegistroPat.jsx
@@ -60,30 +60,93 @@ class App extends Component {
     this.setState({ correo: document.getElementById("correo").value });
     this.setState({ address: document.getElementById("address").value });
     this.setState({ pass: document.getElementById("pass").value });
+
+    this.setState({nomValid : /^[a-zA-Z ]+$/.test(this.state.nombre)});
+    this.setState({apeValid : /^[a-zA-Z ]+$/.test(this.state.apellidos)});
+    this.setState({telValid : /^[0-9]+$/.test(this.state.telefono)});
+    this.setState({corValid : /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(this.state.correo)});
+    this.setState({pasValid : /^(?=.*[A-Za-z])(?=.*\d)(?=.*[-_])[A-Za-z\d@_-]{6,}$/.test(this.state.pass)});
+    
   };
 
   enviar = async (event) => {
     event.preventDefault();
 
-    if (this.state.cuenta == this.state.address) {
-      this.state.contract.methods
-        .addPatient(
-          this.state.nombre,
-          this.state.apellidos,
-          this.state.telefono,
-          this.state.address,
-          this.state.correo,
-          this.state.pass
-        )
-        .send({ from: this.state.cuenta })
-        .then((r) => {
-          window.alert("Registro existoso");
-          window.location.assign("signin");
-        });
+    if (!this.state.nomValid) {
+      //alert("El nombre debe contener solo letras");
+      this.setState({span:"El nombre lleva solo letras"});
+
     } else {
-      window.alert(
-        "La public address ingresada no coincide con la de la cuenta activa de metamask"
-      );
+        this.setState({span:""});
+    }
+
+    if (!this.state.apeValid) {
+      //alert("El nombre debe contener solo letras");
+      this.setState({span2:"El apellido lleva solo letras"});
+
+    } else {
+        this.setState({span2:""});
+    }
+
+    if (!this.state.telValid) {
+      //alert("El nombre debe contener solo letras");
+      this.setState({span3:"El telefono lleva solo letras"});
+
+    } else {
+        this.setState({span3:""});
+    }
+
+    if (!this.state.corValid) {
+      //alert("El nombre debe contener solo letras");
+      this.setState({span4:"No cumple con el formato de un correo electronico"});
+
+    } else {
+        this.setState({span4:""});
+    }
+
+    if (!this.state.pasValid) {
+      //alert("El nombre debe contener solo letras");
+      this.setState({span5:"Debe llevar al menos 1 mayusacula, un signo especial (- _) y almenos 6 caracteres "});
+
+    } else {
+        this.setState({span5:""});
+    }
+
+    
+
+    if (!this.state.nombre || !this.state.apellidos || !this.state.telefono || !this.state.address ||
+         !this.state.correo || !this.state.pass) {
+        alert("Todos los campos son obligatorios");
+    }else{
+      if (this.state.nomValid && this.state.apeValid && this.state.telValid && this.state.corValid &&
+         this.state.pasValid) {
+
+        if (this.state.cuenta == this.state.address) {
+          try {
+          await this.state.contract.methods
+            .addPatient(
+              this.state.nombre,
+              this.state.apellidos,
+              this.state.telefono,
+              this.state.address,
+              this.state.correo,
+              this.state.pass
+            )
+            .send({ from: this.state.cuenta })
+            .then((r) => {
+              window.alert("Registro existoso");
+              window.location.assign("signin");
+            })}catch (error) {
+                           window.alert("Error al crear el usuario, por el siguiente error: " + error.message);
+                          };;
+        } else {
+          alert(
+            "La public address ingresada no coincide con la de la cuenta activa de metamask"
+          );
+        }
+      } else {
+        alert('Llene correctamente los campos')
+      }
     }
   };
 
@@ -99,6 +162,17 @@ class App extends Component {
       cuenta: "",
       sol: [],
       contract: null,
+      nomValid: null,
+      apeValid: null,
+      telValid: null,
+      corValid: null,
+      pasValid: null,
+      span:null,
+      span2:null,
+      span3:null,
+      span4:null,
+      span5:null,
+
     };
   }
 
@@ -121,6 +195,8 @@ class App extends Component {
               id="nombre"
               onChange={this.handleChange}
             />
+            <Typography color="red"> {<span>{this.state.span}</span>} </Typography>
+
             <TextField
               fullWidth
               sx={{ mt: 2 }}
@@ -128,6 +204,8 @@ class App extends Component {
               id="apellidos"
               onChange={this.handleChange}
             />
+            <Typography color="red"> {<span>{this.state.span2}</span>} </Typography>
+
             <TextField
               fullWidth
               sx={{ mt: 2 }}
@@ -135,6 +213,7 @@ class App extends Component {
               id="telefono"
               onChange={this.handleChange}
             />
+            <Typography color="red"> {<span>{this.state.span3}</span>} </Typography>
             <TextField
               fullWidth
               sx={{ mt: 2 }}
@@ -142,6 +221,7 @@ class App extends Component {
               id="correo"
               onChange={this.handleChange}
             />
+            <Typography color="red"> {<span>{this.state.span4}</span>} </Typography>
             <TextField
               fullWidth
               sx={{ mt: 2 }}
@@ -154,8 +234,10 @@ class App extends Component {
               sx={{ mt: 2 }}
               label="Contraseña"
               id="pass"
+              type="password"
               onChange={this.handleChange}
             />
+            <Typography color="red"> {<span>{this.state.span5}</span>} </Typography>
           </FormGroup>
 
           <Stack sx={{ mt: 2, justifyContent: "end" }} direction="row" spacing={2}>
