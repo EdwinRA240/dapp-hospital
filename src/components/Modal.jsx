bb0026b5df8a79060a6d3060669b7bb1aab6c0fd
Edwin RA <edwinra240@gmail.com>
2023-05-10 16:53:27 -0600

    feat: responsive modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,61 +1,57 @@
-import { Modal,
-Typography,
-Button, } from '@mui/material';
-import React from 'react';
+import { Modal, Typography, Button, Container, Box, Grid } from "@mui/material";
+import React from "react";
 
 const MyModal = (props) => {
+  const { open, onClose, message, message2, message3, message4, message5, message6, message7, message8, message9 } =
+    props;
+  const link = "https://ipfs.io/ipfs/" + message9;
 
-  const { open, onClose, message,message2,message3,message4,message5,message6,message7,message8,message9 } = props;
-  const link ='https://ipfs.io/ipfs/'+message9
-  console.log(link)
-  
-
-  const customStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-  
-  const frameStyle = {
-    width: '50%',
-    height: '90%',
-    margin: '10px'
-  };
-
-  const containerStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    width: '80%',
-    height: '80%'
-  };
-
-  const textContainerStyle = {
-    flex: 1,
-    margin: '50px',
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "70%",
+    heigth: "100%",
+    bgcolor: "background.paper",
+    border: "2px solid #0000",
+    boxShadow: 50,
+    p: 4,
   };
 
   return (
-    <Modal open={open} onClose={onClose} style={customStyle}>
-      <div style={containerStyle}>
-        <div style={textContainerStyle}>
-          <Typography variant="h5" align='center'>Expediente Clínico Electrónico</Typography><br/>
-          <Typography>Médico que atendió: {message}</Typography><br/> 
-          <Typography>Nombre del paciente: {message2}</Typography><br/> 
-          <Typography>Edad del paciente: {message3}</Typography><br/> 
-          <Typography>Diagnóstico del paciente: {message4}</Typography><br/> 
-          <Typography>Tratamiento del paciente: {message5}</Typography><br/> 
-          <Typography>Fecha en que se realizó: {message6}</Typography><br/> 
-          <Typography>Estado: {message7}</Typography><br/> 
-          <Typography>Notas adicionales: {message8}</Typography><br/> 
-          <Button onClick={onClose} variant="contained">Cerrar</Button>
-        </div>
-        {message9 !== 'Sin archivo adjunto' ? (
-          <iframe style={frameStyle} src={link} />
-        ) : null}
-      </div>
+    <Modal open={open} onClose={onClose}>
+      <Box sx={style}>
+        <Typography variant="h5" align="center">
+          Expediente Clínico Electrónico
+        </Typography>
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} lg={6}>
+            {/* <Typography sx={{ mt: 1 }}>Médico que atendió: {message}</Typography> */}
+            <Typography sx={{ mt: 1 }}>Nombre del paciente: {message2}</Typography>
+            <Typography sx={{ mt: 1 }}>Edad del paciente: {message3}</Typography>
+            <Typography sx={{ mt: 1 }}>Diagnóstico del paciente: {message4}</Typography>
+            <Typography sx={{ mt: 1 }}>Tratamiento del paciente: {message5}</Typography>
+            <Typography sx={{ mt: 1 }}>Fecha en que se realizó: {message6}</Typography>
+            <Typography sx={{ mt: 1 }}>Estado: {message7}</Typography>
+            <Typography>Notas adicionales: {message8}</Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={6}>
+            {message9 !== "Sin archivo adjunto" ? (
+              <Box sx={{ display: "flex", justifyContent: "center", minWidth: "10px" }}>
+                <iframe src={link} />
+              </Box>
+            ) : (
+              ""
+            )}
+          </Grid>
+        </Grid>
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button onClick={onClose} variant="contained" sx={{ mt: 2 }}>
+            Cerrar
+          </Button>
+        </Box>
+      </Box>
     </Modal>
   );
 };
